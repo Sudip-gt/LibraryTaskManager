6caeb6b7bd381220d6f282afd3c6db4f07fa8bd3
Sudip-gt <sumanledop@gmail.com>
2026-04-30 21:12:49 +0545

    used docker for db

--- server/src/server.ts
+++ server/src/server.ts
@@ -41,6 +41,7 @@ const startServer = async () => {
   const PORT: number = parseInt(process.env.PORT || '5000');
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Frontend URL: ${process.env.CLIENT_URL}`);
   });
 };
 
