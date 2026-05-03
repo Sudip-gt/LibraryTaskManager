8e8d3fba4e16b5d3da6da89e6a49261b6809fc38
Sudip-gt <sumanledop@gmail.com>
2026-05-03 21:37:10 +0545

    db config changed

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -2,10 +2,15 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI!);
-    console.log('MongoDB connected');
+    const conn = await mongoose.connect(process.env.MONGO_URI!, {
+      maxPoolSize: 10,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+    });
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error(err);
+    console.error('MongoDB connection error:', err);
     process.exit(1);
   }
 };
